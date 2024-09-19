8ffb9cddd5aa20e5d2817150cfde897dfb9e4393
david catalan <catalan@adobe.com>
2024-09-19 13:58:10 +0200

    Add accepted blocks from AEM Block Assistant

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,18 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  // Add a new class to the block
+  block.classList.add('cards');
+
+  // Iterate over each card and apply necessary class
+  block.querySelectorAll('.cards > div').forEach((card) => {
+    card.classList.add('card');
+    const imageContainer = card.querySelector('picture');
+    const description = card.querySelector('p');
+    const title = card.querySelector('h3');
+    const link = card.querySelector('p a');
+
+    imageContainer.classList.add('card-image');
+    description.classList.add('card-description');
+    title.classList.add('card-title');
+    link.classList.add('card-link');
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
 }
--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,24 @@
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  block.classList.add('columns');
 
-  // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
+  const columns = Array.from(block.querySelectorAll('.columns > div > div'));
+  columns.forEach((columnDiv, index) => {
+    columnDiv.classList.add(`column-${index + 1}`);
   });
+
+  // Ensure block has three columns
+  const column1 = document.createElement('div');
+  const column2 = document.createElement('div');
+  const column3 = document.createElement('div');
+
+  column1.classList.add('column');
+  column2.classList.add('column');
+  column3.classList.add('column');
+
+  column1.append(...columns[0].children);
+  column2.append(...columns[1].children);
+  column3.append(...columns[2].children);
+
+  block.innerHTML = '';
+  block.append(column1, column2, column3);
 }
